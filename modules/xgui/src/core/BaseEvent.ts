9180713263cb02add7b831942a1ac3eaf441f10c
Nidin Vinayakan <nidin.vinayakan@3ds.com>
2015-10-02 22:48:58 +0200

    Base structure

---
+++ modules/xgui/src/core/BaseEvent.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by Nidin Vinayakan on 02-10-2015.
+ */
+module core {
+
+    export class BaseEvent{
+
+        type:string;
+
+        constructor(type:string) {
+            this.type = type;
+        }
+    }
+}
---
+++ modules/xgui/src/events/InputEvent.ts
@@ -0,0 +1,5 @@
+
+/**
+ * Created by Nidin Vinayakan on 02-10-2015.
+ */
+
