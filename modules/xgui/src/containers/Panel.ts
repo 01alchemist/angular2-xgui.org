3d147fced169171e5d305deed140067478b487a9
Nidin Vinayakan <01@01alchemist.com>
2016-06-04 22:55:25 +0200

    Migrated to angular rc.1

--- modules/xgui/src/containers/Panel.ts
+++ modules/xgui/src/containers/Panel.ts
@@ -1,103 +1,104 @@
-import {Component, EventEmitter, ElementRef, ComponentRef/*, ViewContainerRef*/} from '@angular/core';
-import {NgFor,NgIf} from '@angular/common';
+import {
+    Component, EventEmitter, ElementRef, ViewContainerRef,
+    ApplicationRef/*, ViewContainerRef*/
+} from '@angular/core';
+import {NgFor, NgIf} from '@angular/common';
 import {Icon} from "../controls/Icon";
 import {Assets} from "../assets/Assets";
 import {StyleUtils} from "../utils/StyleUtils";
-import {ViewResolver} from "@angular/compiler";
 
 /**
  * Created by Nidin Vinayakan on 02-10-2015.
  */
 @Component({
-    selector:'x-panel',
-    properties:['name:name'],
-    template:
-    '<div class="panel-container">' +
-        '<div class="stack-header">' +
-            '<div class="close-button" (click)="close($event)"><x-icon class="stack-close-icon" [src]="closeIcon"></x-icon></div>' +
-            '<div class="expand-button"><x-icon class="expand-arrow-icon" [src]="arrowIcon"></x-icon></div>' +
-        '</div>' +
-        '<div class="panel-header">' +
-        '<div class="panel-title" [style.width]="titleWidth" [style.height]="titleHeight">{{name}}</div>' +
-        '</div>' +
-        '<div class="stack-tabs">' +
-        '<ng-content></ng-content>' +
-        '</div>' +
+    selector: 'x-panel',
+    properties: ['name:name'],
+    template: '<div class="panel-container">' +
+    '<div class="stack-header">' +
+    '<div class="close-button" (click)="close($event)"><x-icon class="stack-close-icon" [src]="closeIcon"></x-icon></div>' +
+    '<div class="expand-button"><x-icon class="expand-arrow-icon" [src]="arrowIcon"></x-icon></div>' +
+    '</div>' +
+    '<div class="panel-header">' +
+    '<div class="panel-title" [style.width]="titleWidth" [style.height]="titleHeight">{{name}}</div>' +
+    '</div>' +
+    '<div class="stack-tabs">' +
+    '<ng-content></ng-content>' +
+    '</div>' +
     '</div>',
     styles: [
         'x-panel{' +
-            'min-width:212px;' +
-            'max-width:1370px;' +
-            'min-height:148px;' +
-            'max-height:805px;' +
-            'display: block;' +
-            'border: 1px solid #282828;' +
-            'background-color: #535353;' +
+        'min-width:212px;' +
+        'max-width:1370px;' +
+        'min-height:148px;' +
+        'max-height:805px;' +
+        'display: block;' +
+        'border: 1px solid #282828;' +
+        'background-color: #535353;' +
         '}',
         '.stack-header{' +
-            'display:block;' +
-            'cursor:default;' +
-            'width:inherit;' +
-            'height:11px;' +
-            'border-top: #474747 1px solid;' +
-            'border-bottom: #282828 1px solid;' +
-            'background-color: #323232;' +
+        'display:block;' +
+        'cursor:default;' +
+        'width:inherit;' +
+        'height:11px;' +
+        'border-top: #474747 1px solid;' +
+        'border-bottom: #282828 1px solid;' +
+        'background-color: #323232;' +
         '}',
         '.expand-arrow-icon{' +
-            'width:7px;' +
-            'height:5px;' +
-            'top:3px;' +
-            'left:2px;' +
+        'width:7px;' +
+        'height:5px;' +
+        'top:3px;' +
+        'left:2px;' +
         '}',
         '.expand-button{' +
-            'cursor: pointer;' +
-            'width: 12px;' +
-            'height: 12px;' +
-            'float:right;' +
+        'cursor: pointer;' +
+        'width: 12px;' +
+        'height: 12px;' +
+        'float:right;' +
         '}',
         '.expand-button:hover{' +
-            'background-color:#222222;' +
+        'background-color:#222222;' +
         '}',
         '.stack-close-icon{' +
-            'width:6px;' +
-            'height:5px;' +
-            'top:3px;' +
-            'left:3px;' +
+        'width:6px;' +
+        'height:5px;' +
+        'top:3px;' +
+        'left:3px;' +
         '}',
         '.close-button{' +
-            'cursor: pointer;' +
-            'width: 12px;' +
-            'height: 12px;' +
-            'float:right;' +
+        'cursor: pointer;' +
+        'width: 12px;' +
+        'height: 12px;' +
+        'float:right;' +
         '}',
         '.close-button:hover{' +
-            'background-color:#222222;' +
+        'background-color:#222222;' +
         '}',
         '.panel-header{' +
-            'display:block;' +
-            'width:inherit;' +
-            'height:25px;' +
-            'border-top: #282828 1px solid;' +
-            'background-color: #8A8782;' +
-            'border-bottom: #444444 1px solid;' +
+        'display:block;' +
+        'width:inherit;' +
+        'height:25px;' +
+        'border-top: #282828 1px solid;' +
+        'background-color: #8A8782;' +
+        'border-bottom: #444444 1px solid;' +
         '}',
         '.panel-title{' +
-            'width:70px;' +
-            'height:15px;' +
-            'padding:5px;' +
-            'padding-left:8px;' +
-            'color:#ffffff;' +
-            'font-size:0.94em;' +
-            'font-weight:bold;' +
-            'text-shadow: 0 -1px #000;' +
-            'background-color: #6F6F6F;' +
-            'font-family: Arial;' +
-            /*'text-transform: uppercase;' +*/
+        'width:70px;' +
+        'height:15px;' +
+        'padding:5px;' +
+        'padding-left:8px;' +
+        'color:#ffffff;' +
+        'font-size:0.94em;' +
+        'font-weight:bold;' +
+        'text-shadow: 0 -1px #000;' +
+        'background-color: #6F6F6F;' +
+        'font-family: Arial;' +
+        /*'text-transform: uppercase;' +*/
         '}'
     ],
-    directives:[/*ViewContainerRef, */ViewResolver, NgIf, NgFor, Icon]
+    directives: [/*ViewContainerRef, */NgIf, NgFor, Icon]
 })
-export class Panel{
+export class Panel {
 
     arrowIcon = Assets.icon_arrow_left_7x5;
     closeIcon = Assets.icon_close_6x5;
@@ -108,32 +109,38 @@ export class Panel{
     _elementRef:ElementRef;
     /*_containerRef:ViewContainerRef;*/
 
-    get stackHeadDisplay(){
-        return this.stacked?"none":"block";
+    static id:number = 0;
+
+    get stackHeadDisplay() {
+        return this.stacked ? "none" : "block";
     }
 
-    constructor(elementRef: ElementRef, private _viewResolver:ViewResolver /*, containerRef:ViewContainerRef*/) {
+    private id:number;
+
+    constructor(elementRef:ElementRef, private viewContainerRef:ViewContainerRef, private appRef:ApplicationRef ) {
         this._elementRef = elementRef;
-        this._elementRef = _viewResolver.resolve(this);
         /*this._containerRef = containerRef;*/
-        this._elementRef.nativeElement.addEventListener("mousedown",this.handleMouseEvent);
-        this._elementRef.nativeElement.addEventListener("mouseup",this.handleMouseEvent);
-        this._elementRef.nativeElement.addEventListener("onmouseleave",this.handleMouseEvent);
-        document.addEventListener("onmouseout",this.handleMouseEvent);
-        this._elementRef.nativeElement.addEventListener("onmousemove",this.handleMouseEvent);
+        this._elementRef.nativeElement.addEventListener("mousedown", this.handleMouseEvent);
+        this._elementRef.nativeElement.addEventListener("mouseup", this.handleMouseEvent);
+        this._elementRef.nativeElement.addEventListener("onmouseleave", this.handleMouseEvent);
+        document.addEventListener("onmouseout", this.handleMouseEvent);
+        this._elementRef.nativeElement.addEventListener("onmousemove", this.handleMouseEvent);
 
-        setInterval(function(){
-            console.log("I am alive");
-        },2000);
+        this.id = ++Panel.id;
     }
-    handleMouseEvent(event){
+
+    handleMouseEvent(event) {
         console.log(event.type);
     }
-    close(){
-        this.componentRef.destroy();
+
+    close() {
+        //this.viewContainerRef.remove(1);
+        this._elementRef.nativeElement.remove();
+        // this.viewContainerRef["_element"].componentView.ref.destroy();
+        // this.viewContainerRef["_element"].componentView.destroy();
         /*var eli = this._elementRef.parentView._view.elementInjectors[this._elementRef.boundElementIndex];
-        var vc = eli.getHost().getViewContainerRef();
-        vc.remove(this._elementRef.boundElementIndex);*/
+         var vc = eli.getHost().getViewContainerRef();
+         vc.remove(this._elementRef.boundElementIndex);*/
     }
 }
 
